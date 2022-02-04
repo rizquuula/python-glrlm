099bb6a7e79ddfc9fde31e0ca44c5d2297cc849f
Muhammad Razif Rizqullah <razifrizqullah@gmail.com>
2022-02-04 13:07:13 +0700

    update setup with new url

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-from telnetlib import Telnet
 from setuptools import setup
 import pathlib
 
@@ -7,22 +6,32 @@ README = (BASE_DIR / "README.md").read_text()
 
 setup(
     name="glrlm",
-    version="0.1.0",
+    version="0.0.2",
     author="M Razif Rizqullah",
     author_email="razifrizqullah@gmail.com",
+    url='https://github.com/eiproject/python-glrlm',
     description="Gral Level Run Length Matrix",
     long_description=README,
+    long_description_content_type="text/markdown",
     license='MIT',
     packages=['glrlm'],
     include_package_data=True,
-    install_requires=[],
-    keywords=['python', 'glrlm', 'feature extraction'],
+    install_requires=[
+            'numpy',
+            'opencv-python'
+            ],
+    keywords=['python', 'glrlm', 'feature extraction', 'image processing'],
     classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
         "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
     ]
 )
+
+
+# python setup.py sdist bdist_wheel
+# twine upload dist/*
